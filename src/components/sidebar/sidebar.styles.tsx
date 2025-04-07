6f5e69b3668c2ba9dd2c26b74d907c9f02f086da
KishanthDev <kishanthkisna@gmail.com>
2025-04-07 18:26:53 +0530

    fixed sidebar styling

--- src/components/sidebar/sidebar.styles.tsx
+++ src/components/sidebar/sidebar.styles.tsx
@@ -1,34 +1,26 @@
 import React from "react";
 import { Box } from "../styles/box"; // Assuming you have this helper
 
-export const SidebarWrapper = ({ collapsed }: { collapsed: boolean }) => {
-  return (
-    <div
-      className={`bg-background fixed h-full w-64 overflow-y-auto transform transition-transform duration-200 z-202 flex flex-col py-10 px-6 border-r border-border md:flex md:static md:h-screen md:translate-x-0 ${collapsed ? "translate-x-0" : "-translate-x-full"}`}
-    >
-    </div>
-  );
-};
+export const SidebarWrapper = ({ collapsed }: { collapsed: boolean }) => (
+  <div
+    className={`z-202 fixed flex h-full w-64 transform flex-col overflow-y-auto border-r border-border bg-background px-6 py-10 transition-transform duration-200 md:static md:flex md:h-screen md:translate-x-0 ${collapsed ? "translate-x-0" : "-translate-x-full"}`}
+  ></div>
+);
 
 export const Overlay = () => (
-  <div
-    className="fixed inset-0 bg-black bg-opacity-30 z-201 transition-opacity duration-300 opacity-80 md:hidden md:z-auto md:opacity-100"
-  />
+  <div className="z-201 fixed inset-0 bg-black bg-opacity-30 opacity-80 transition-opacity duration-300 md:z-auto md:hidden md:opacity-100" />
 );
 
 export const Header = () => (
-  <div className="flex gap-8 items-center px-10">
-  </div>
+  <div className="flex items-center gap-8 px-10"></div>
 );
 
 export const Body = () => (
-  <div className="flex flex-col gap-10 mt-13 px-4">
-  </div>
+  <div className="mt-13 flex flex-col gap-10 px-4"></div>
 );
 
 export const Footer = () => (
-  <div className="flex items-center justify-center gap-12 pt-18 pb-8 md:pt-0 md:pb-0 px-8">
-  </div>
+  <div className="pt-18 flex items-center justify-center gap-12 px-8 pb-8 md:pb-0 md:pt-0"></div>
 );
 
 export const Sidebar = ({ collapsed }: { collapsed: boolean }) => (
