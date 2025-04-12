af783bb3853d5a71e5dfb28d7f55d18c75838f0b
KishanthDev <kishanthkisna@gmail.com>
2025-04-12 13:46:40 +0530

    refactor: remove unused analytics and meta tags from RootLayout; streamline layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Layout } from "@/components/layout/layout";
 import classNames from "classnames";
@@ -18,16 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#e0c8fd" />
-        <meta property="og:title" content="Nextjs App" />
-        <meta name="twitter:title" content="Nextjs App" />
-        <meta name="description" content="Nextjs 14 starter template" />
-        <meta property="og:description" content="Nextjs 14 starter template" />
-        <meta name="twitter:description" content="Nextjs 14 starter template" />
-        <meta property="og:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
-        <meta name="twitter:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
       </head>
       <body
         className={classNames("antialiased", {
@@ -42,7 +31,6 @@ export default function RootLayout({
         >
           <Layout>{children}</Layout>
         </ThemeProvider>
-        <Analytics />
       </body>
     </html>
   );
